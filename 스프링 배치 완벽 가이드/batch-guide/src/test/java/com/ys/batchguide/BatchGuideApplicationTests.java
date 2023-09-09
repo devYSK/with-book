06fe0b98c6521b989f9858ae931a9945f3e7aa04
devYSK <kim206gh@naver.com>
2023-09-09 20:46:18 +0900

    add : 2장 스프링 배치 맛보기 정리

---
+++ 스프링 배치 완벽 가이드/batch-guide/src/test/java/com/ys/batchguide/BatchGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.batchguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
